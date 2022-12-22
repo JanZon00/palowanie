56914b9c9274a63fe651f7595a4287e0d741d1fd
JanZon00 <112341667+JanZon00@users.noreply.github.com>
2022-12-22 23:06:04 +0100

    client server

---
+++ client/vue.config.js
@@ -0,0 +1,21 @@
+const { defineConfig } = require('@vue/cli-service');
+const path = require('path');
+
+module.exports = {
+  transpileDependencies: true,
+  outputDir: path.resolve(__dirname, '../server/public'),
+  pluginOptions: {
+    vuetify: {
+			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+		}
+  },
+  devServer: {
+    /*proxy: {
+      '/api' : {
+        target: "http://localhost:5000",
+        changeOrigin: true
+      },
+    }*/
+    proxy: "http://localhost:5000"
+  }
+};
